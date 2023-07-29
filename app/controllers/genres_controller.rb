346e42811d87f06dc8dee2586f941fc750d0d0f4
Tacud1810 <r.kachyna@gmail.com>
2023-07-29 19:17:24 +0200

    Přidána tabulka se žánry. Doplněny základní žánry do dtb. Nastaven layout aplikace.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,50 @@
+class GenresController < ApplicationController
+	before_action :set_genre, only:[:show, :update, :edit, :destroy]
+
+	def show
+	end
+
+	def index
+		@genres = Genre.all
+	end
+	def new
+		@genre = Genre.new
+	end
+
+	def update
+		if @genre.update(genre_params)
+			flash[:notice] = "Update succesfull"
+			redirect_to genres_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	def edit
+	end
+
+	def create
+		@genre = Genre.new(genre_params)
+		# @genre.user = User.first
+		if @genre.save
+			flash[:notice] = "Genre was created succesfully."
+			redirect_to @genre
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@genre.destroy
+		redirect_to genres_path
+	end
+
+	private
+	def set_genre
+		@genre = Genre.find(params[:id])
+	end
+
+	def genre_params
+		params.require(:genre).permit(:name, :description)
+	end
+end
---
+++ app/models/genre.rb
@@ -0,0 +1,4 @@
+class Genre < ApplicationRecord
+	validates :name, presence:true
+	validates :description, presence:true
+end
